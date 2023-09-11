1b8bd882aeb60f7ceff5abc4eedac362a44365a1
Kevin Steven Arias Duarte <kariasduarte@gmail.com>
2023-09-11 18:09:51 -0500

    archivos de llamado de nodos

---
+++ src/node.js
@@ -0,0 +1,31 @@
+/* Acá seleccionamos cada elemento de html con javascript para poder hacer las
+rutas con location */
+
+/*Header */
+
+const headerLabelInput = document.querySelector('#headerLabelInput')
+
+/*Nav */
+
+const nav = document.querySelector('.nav');
+
+/*Nav Arrow */
+
+const navArrow = document.querySelector('.navArrow');
+const navArrowBack = document.querySelector('#navArrowBack');
+
+/*Section1 La parte de tendencias tiene las clases repetidas*/
+
+const section = document.querySelector('.section');
+const estrenos = document.querySelector('#estrenos');
+const tendencia = document.querySelector('#tendencia');
+const sectionEstrenos = document.querySelector('#sectionEstrenos');
+const sectionTendencias = document.querySelector('#sectionTendencias');
+
+/*Section2 También tiene clases repetidas*/
+
+const section2 = document.querySelector('.section2');
+
+/*Section3 */
+
+const section3 = document.querySelector('.section3');
